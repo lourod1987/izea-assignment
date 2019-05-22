f759df9294e9a4ca07102e1a1e69716d49b85e78
lourod1987 <louart87@gmail.com>
2019-05-22 16:22:42 -0500

    Feat - set up model to make a paginated call to the api using store.query to call page specific resources

---
+++ app/routes/page4.js
@@ -0,0 +1,11 @@
+import Route from '@ember/routing/route';
+import { set } from '@ember/object';
+
+export default Route.extend({
+  model() {
+    return this.store.query('post', { _page: 4, _limit: 20 });
+  },
+  setupController(controller, model) {
+    set(controller, 'posts', model)
+  },
+});
