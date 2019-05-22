7f2c4ca789796e52daca52628f1c24067dd1e1e4
lourod1987 <louart87@gmail.com>
2019-05-22 16:20:20 -0500

    Feat - set up model to make a paginated call to the api using store.query

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,9 +3,10 @@ import { set } from '@ember/object';
 
 export default Route.extend({
   model() {
-    return this.store.findAll('post');
+    return this.store.query('post', { _page: 1, _limit: 20 });
   },
   setupController(controller, model) {
     set(controller, 'posts', model)
-  }
+  },
 });
+
